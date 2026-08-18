7e83d8173745a3505777f72031e8bc95cbc91d89
Wongsapat <biga.photo22@gmail.com>
2026-08-18 11:43:46 +0700

    Add sidebar navigation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,83 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ManaChaiLeasing
+namespace ManaChaiLeasing;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private void HomeButton_Click(object sender, RoutedEventArgs e)
+    {
+        ShowPage(
+            HomeContent,
+            HomeButton,
+            "หน้าหลัก",
+            "ระบบบันทึกข้อมูลลูกค้าและรายการรับจำนำ");
+    }
+
+    private void NewPawnButton_Click(object sender, RoutedEventArgs e)
+    {
+        ShowPage(
+            NewPawnContent,
+            NewPawnButton,
+            "รับจำนำใหม่",
+            "บันทึกข้อมูลลูกค้า สินค้า หมายเลขตั๋ว และยอดเงิน");
+    }
+
+    private void SearchButton_Click(object sender, RoutedEventArgs e)
+    {
+        ShowPage(
+            SearchContent,
+            SearchButton,
+            "ค้นหารายการ",
+            "ค้นหาข้อมูลและประวัติรายการย้อนหลัง");
+    }
+
+    private void TodayButton_Click(object sender, RoutedEventArgs e)
+    {
+        ShowPage(
+            TodayContent,
+            TodayButton,
+            "รายการวันนี้",
+            "สรุปรายการรับจำนำ ต่อดอก ไถ่ถอน และยอดประจำวัน");
+    }
+
+    private void SettingsButton_Click(object sender, RoutedEventArgs e)
+    {
+        ShowPage(
+            SettingsContent,
+            SettingsButton,
+            "ตั้งค่า",
+            "ตั้งค่าข้อมูลร้านและเงื่อนไขการใช้งานระบบ");
+    }
+
+    private void ShowPage(
+        UIElement pageToShow,
+        Button activeButton,
+        string pageTitle,
+        string pageSubtitle)
+    {
+        HomeContent.Visibility = Visibility.Collapsed;
+        NewPawnContent.Visibility = Visibility.Collapsed;
+        SearchContent.Visibility = Visibility.Collapsed;
+        TodayContent.Visibility = Visibility.Collapsed;
+        SettingsContent.Visibility = Visibility.Collapsed;
+
+        pageToShow.Visibility = Visibility.Visible;
+
+        HomeButton.Style = (Style)FindResource("SidebarButtonStyle");
+        NewPawnButton.Style = (Style)FindResource("SidebarButtonStyle");
+        SearchButton.Style = (Style)FindResource("SidebarButtonStyle");
+        TodayButton.Style = (Style)FindResource("SidebarButtonStyle");
+        SettingsButton.Style = (Style)FindResource("SidebarButtonStyle");
+
+        activeButton.Style = (Style)FindResource("SidebarActiveButtonStyle");
+
+        PageTitleText.Text = pageTitle;
+        PageSubtitleText.Text = pageSubtitle;
     }
 }
